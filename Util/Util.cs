c14c71f7b7b308940506c95a47c32cede4d30ad2
Disaac1 <disaac1256@gmail.com>
2023-08-15 17:01:43 -0500

    Added Tree command
    Command Used to list all Files and subfiles of a location. Used in debugging pck loading

---
+++ Util/Util.cs
@@ -0,0 +1,24 @@
+﻿using Godot;
+
+public class Util
+{
+    public static void Tree(string path = "res://", int level = 1)
+    {
+        DirAccess dir = DirAccess.Open(path);
+
+        foreach (string item in dir.GetFiles())
+        {
+            GD_ignore.Print("---------------------------------------".Substr(0,level)+item);
+        }
+        foreach (string item in dir.GetDirectories())
+        {
+            GD_ignore.Print("---------------------------------------".Substr(0,level)+item);
+            if(item.StartsWith("."))
+            {
+                GD_ignore.Print("------------------------------------------".Substring(0, level+1) + " Skipping");
+                continue;
+            }
+            Tree(path + "/" + item, level + 1);
+        }
+    }
+}
