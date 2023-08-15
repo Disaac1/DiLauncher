28b3d5b0d72260323dabc3036a282dfb8da5c193
Disaac1 <disaac1256@gmail.com>
2023-08-15 17:06:10 -0500

    Fixed a refactoring issue
    GD was autoreplaced

--- Util/Util.cs
+++ Util/Util.cs
@@ -8,14 +8,14 @@ public class Util
 
         foreach (string item in dir.GetFiles())
         {
-            GD_ignore.Print("---------------------------------------".Substr(0,level)+item);
+            GD.Print("---------------------------------------".Substr(0,level)+item);
         }
         foreach (string item in dir.GetDirectories())
         {
-            GD_ignore.Print("---------------------------------------".Substr(0,level)+item);
+            GD.Print("---------------------------------------".Substr(0,level)+item);
             if(item.StartsWith("."))
             {
-                GD_ignore.Print("------------------------------------------".Substring(0, level+1) + " Skipping");
+                GD.Print("------------------------------------------".Substring(0, level+1) + " Skipping");
                 continue;
             }
             Tree(path + "/" + item, level + 1);
